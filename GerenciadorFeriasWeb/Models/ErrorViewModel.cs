5b27c29322491a41f9bf26867c3a89bef9545636
Tiago Casagrande <81438225+tiagocasa@users.noreply.github.com>
2022-08-11 10:14:55 -0300

    Add project files.

---
+++ GerenciadorFeriasWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GerenciadorFeriasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
