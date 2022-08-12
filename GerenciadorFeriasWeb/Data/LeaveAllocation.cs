e8d4a170dbe722fa9f1fa0c12c8dce75daac0fea
Tiago Casagrande <81438225+tiagocasa@users.noreply.github.com>
2022-08-11 23:25:23 -0300

    Added LeaveType and LeaveAllocation tables

---
+++ GerenciadorFeriasWeb/Data/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciadorFeriasWeb.Data
+{
+    public class LeaveAllocation
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+
+
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ GerenciadorFeriasWeb/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿namespace GerenciadorFeriasWeb.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
